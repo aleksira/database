8bf9cf12e0754e27148ef885743e520a9e623625
Alexandra Berezovskaya <akberezovskaya@mail.ru>
2024-09-20 15:03:22 +0300

    Change file

--- client_database_hw.py
+++ client_database_hw.py
@@ -1,35 +1,41 @@
 import psycopg2
 
 
-# Создание таблицы:
+# Создание таблицы клиентов и связанной с ней таблицы телефонов:
 def create_table(conn):
     conn.cursor().execute('''
         CREATE TABLE IF NOT EXISTS client(
             id SERIAL PRIMARY KEY,
             first_name TEXT NOT NULL,
             last_name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            phone TEXT
+            email TEXT NOT NULL
+        );
+        
+        CREATE TABLE IF NOT EXISTS phones(
+            id SERIAL PRIMARY KEY,
+            phone VARCHAR(40) UNIQUE,
+            client_id INTEGER NOT NULL REFERENCES client(id)
         );
         ''')
-    conn.commit()
+    pass
 
 
 # Добавление нового клиента:
-def new_client(conn, first_name, last_name, email, phones=None):
+def new_client(conn, first_name, last_name, email):
     conn.cursor().execute('''
-        INSERT INTO client(first_name, last_name, email, phone)
-        VALUES(%s, %s, %s, %s);
-        ''', (first_name, last_name, email, phones))
-    conn.commit()
+        INSERT INTO client(first_name, last_name, email)
+        VALUES(%s, %s, %s);
+        ''', (first_name, last_name, email))
+    pass
 
 
 # Добавление телефона по id клиента:
 def add_phone(conn, phone, client_id):
     conn.cursor().execute('''
-        UPDATE client SET phone=%s WHERE id=%s;
+        INSERT INTO phones(phone, client_id)
+        VALUES(%s, %s);
         ''', (phone, client_id))
-    return conn.commit()
+    pass
 
 
 # Вспомогательная функция: возвращает информацию из запрашиваемого столбика по id клиента
@@ -50,34 +56,23 @@ def change_info(conn, client_id, first_name=None, last_name=None, email=None, ph
         last_name = info_by_id(conn, client_id, 'last_name')
     if email is None:
         email = info_by_id(conn, client_id, 'email')
-    if phones is None:
-        phones = info_by_id(conn, client_id, 'phone')
+    if phones is not None:
+        conn.cursor.execute('''
+            UPDATE phones SET phone=%s WHERE client_id=%s;
+        ''', (phones, client_id))
     conn.cursor().execute('''
-        UPDATE client SET first_name=%s, last_name=%s, email=%s, phone=%s WHERE id=%s;
-        ''', (first_name, last_name, email, phones, client_id))
-    conn.commit()
+        UPDATE client SET first_name=%s, last_name=%s, email=%s WHERE id=%s;
+        ''', (first_name, last_name, email, client_id))
+    pass
+
 
 # Удаление телефона:
 def delete_phone(conn, client_id, phone):
-    # сначала получаем уже имеющуюся информацию о телефонах
-    conn.cursor().execute('''
-        SELECT phone
-        FROM client
-        WHERE id=$s;
-        ''', (client_id,))
-    phones = conn.cursor().fetchone()[0]
-
-    # перезаписываем строку с телефонами
-    if phone in phones:
-        phones.replace(phone, '')
-
-    # обновляем информацию в базе данных
-    conn.cursor().execute('''
-        UPDATE client
-        SET phone=%s
-        WHERE id=%s;
-        ''', (phones, client_id))
-    conn.commit()
+    conn.cursor.execute('''
+        DELETE FROM phones
+        WHERE phone=%s and client_id=%s;
+    ''', (phone, client_id))
+    pass
 
 
 # Удаление клиента по id
@@ -86,63 +81,53 @@ def delete_client(conn, client_id):
         DELETE FROM client
         WHERE id=%s;
         ''', (client_id,))
-    conn.commit()
+    pass
 
 
 # Поиск id клиента по его данным:
 def find_client(conn, first_name=None, last_name=None, email=None, phones=None):
-    if first_name is not None:
-        conn.cursor().execute('''
-            SELECT id
-            FROM client
-            WHERE first_name=%s;
-            ''', (first_name,))
-        client_id = conn.cursor().fetchone()[0]
+    if first_name is None:
+        first_name = '%'
+    if last_name is None:
+        last_name = '%'
+    if email is None:
+        email = '%'
 
-    elif last_name is not None:
-        conn.cursor().execute('''
-            SELECT id
-            FROM client
-            WHERE last_name=%s;
-            ''', (last_name,))
-        client_id = conn.cursor().fetchone()[0]
+    conn.cursor.execute('''
+        SELECT id FROM client
+        WHERE first_name LIKE %s AND last_name LIKE %s AND email LIKE %s;
+    ''', (first_name, last_name, email))
 
-    elif email is not None:
-        conn.cursor().execute('''
-            SELECT id
-            FROM client
-            WHERE email=%s;
-            ''', (email,))
-        client_id = conn.cursor().fetchone()[0]
+    client_id = conn.cursor.fetchall()
 
-    elif phones is not None:
-        conn.cursor().execute('''
-            SELECT id
-            FROM client
-            WHERE phone LIKE f'%{%s}%';
-            ''', (phones,))
-        client_id = conn.cursor().fetchone()[0]
+    if phones is not None:
+        conn.cursor.execute('''
+            SELECT client_id from phones
+            WHERE phone=%s;
+        ''', (phones,))
+        client_id = conn.cursor().fetchall()
 
-    else:
-        client_id = 'not found'
     return client_id
 
 
 # Реализация функций:
-with psycopg2.connect(database='clients', user='postgres', password='gottawork') as conn:
-    conn.cursor().execute('''
-        DROP TABLE IF EXISTS client;
-        ''')
-    conn.commit()
+if __name__ == '__main__':
+    with psycopg2.connect(database='clients', user='postgres', password='gottawork') as conn:
+        conn.cursor().execute('''
+            DROP TABLE IF EXISTS client;
+            DROP TABLE IF EXISTS phones;
+            ''')
+        conn.commit()
 
-    create_table(conn)
-    new_client(conn, 'Albert', 'Brown', 'alb@em.com', '1234567')
-    new_client(conn, 'Robert', 'Lee', 'rob@em.com')
-    add_phone(conn, '098890', 2)
-    change_info(conn, 1, email='alb02@em.com')
-    delete_phone(conn, 1, '1234567')
-    delete_client(conn, 1)
-    robert_id = find_client(conn, first_name='Robert')
-    print(robert_id)
+        create_table(conn)
+        new_client(conn, 'Albert', 'Brown', 'alb@em.com')
+        new_client(conn, 'Robert', 'Lee', 'rob@em.com')
+        add_phone(conn, '1323332', 1)
+        add_phone(conn, '098890', 2)
+        change_info(conn, 1, email='alb02@em.com')
+        delete_phone(conn, 2, '098890')
+        delete_client(conn, 1)
+        robert_id = find_client(conn, first_name='Robert')
+        print(robert_id)
 
-conn.close()
+    conn.close()
